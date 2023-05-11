adb93523db42a5aa4d37bba88b77acf41b599be8
kamranss <kamranss@code.edu.az>
2023-05-11 23:23:40 +0400

    Refactor: Task updated

--- WebApplication1/WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -4,7 +4,7 @@ namespace WebApplication1.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        public IActionResult login()
         {
             return View();
         }
