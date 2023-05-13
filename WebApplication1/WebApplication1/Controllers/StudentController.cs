4c694d4ea76f6b52cd602e49bc7dbb654ed12e34
kamranss <kamranss@code.edu.az>
2023-05-13 15:17:36 +0400

    Refactor: Student view added

--- WebApplication1/WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        public IActionResult StudentIndex()
         {
-            return View();
+
+            ViewData["ExcludeCSS"] = true;
+
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Kamran",
+                Age = 12,
+                GroupId = 1,
+            };
+            Student student2 = new Student()
+            {
+                Id = 2,
+                Name = "Test",
+                Age = 1,
+                GroupId = 1,
+            };
+            Student student3 = new Student()
+            {
+                Id = 3,
+                Name = "Test",
+                Age = 1,
+                GroupId = 1,
+            };
+
+            List<Student> studentList = new List<Student>();
+            studentList.Add(student);
+            studentList.Add(student2);
+            studentList.Add(student3);
+
+            Group Group = new Group()
+            {
+                Id = 1,
+                Name = "Test",
+            };
+            HomeVM HomeVM = new HomeVM()
+            {
+                Students = studentList,
+                Group = Group
+            };
+
+
+            return View(HomeVM);
         }
         public IActionResult Detail(int id)
         {
